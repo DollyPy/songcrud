46f208c76ec8750f4f5a5512e20583534c6e5c54
DollyPy <ddolapo1@gmail.com>
2022-10-29 06:09:30 +0100

    Added Models to Musicapp

--- musicapp/admin.py
+++ musicapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Artiste, Song
 # Register your models here.
+@admin.register(Artiste)
+class ArtisteAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "age")
+
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+    list_display = ("title", "singer", "date_released")
+    ordering = ["-date_released"]
+
